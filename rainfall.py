e2a0717717e7d75e6ee2698a2cee30dd82e1f786
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-24 12:15:36 +0200

    gitautosync update 49

--- rainfall.py
+++ rainfall.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+rainfall = {}
+
 while True:
     city_name = input("City: ").strip()
     if not city_name:
         break
 
     mm_rain = input("Rain: ").strip()
+    rainfall[city_name] += mm_rain
