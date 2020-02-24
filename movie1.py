83763fd61c52a7af6c7d90caab3a71e251658948
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-24 14:19:10 +0200

    gitautosync update 83

--- movie1.py
+++ movie1.py
@@ -13,6 +13,7 @@ for one_movie in movies:
     if one_movie[0] == look_for:
         print(f"Found {look_for}, {one_movie[1]} min, dir {one_movie[2]}")
         found = True
+        break
 
 if not found:
     print(f"Sorry, didn't find {look_for}")
