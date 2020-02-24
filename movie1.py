b401436227c576cbc15544aa0cdea49f6e5fd6ab
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-24 14:16:11 +0200

    gitautosync update 78

---
+++ movie1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# list of lists
+
+movies = [['Spotlight', 129, 'Tom McCarthy'],
+          ['The Big Short', 130, 'Adam McKay'],
+          ['The Martian', 141, 'Ridley Scott']]
