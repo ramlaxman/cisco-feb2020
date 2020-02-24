ecf0d20fd45e1158a8d63760fd84ce6a94c28671
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-24 14:20:33 +0200

    gitautosync update 86

--- movie1.py
+++ movie1.py
@@ -8,11 +8,9 @@ movies = [['Spotlight', 129, 'Tom McCarthy'],
 
 look_for = input("Enter movie title: ")
 
-found = False
 for one_movie in movies:
     if one_movie[0] == look_for:
         print(f"Found {look_for}, {one_movie[1]} min, dir {one_movie[2]}")
-        found = True
         break
 else:  # no break encountered in the for loop
     print(f"Sorry, didn't find {look_for}")
