084d1f9b67be7fd212407abc9c5f1b40e8a1986d
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-24 14:17:06 +0200

    gitautosync update 80

--- movie1.py
+++ movie1.py
@@ -5,3 +5,9 @@
 movies = [['Spotlight', 129, 'Tom McCarthy'],
           ['The Big Short', 130, 'Adam McKay'],
           ['The Martian', 141, 'Ridley Scott']]
+
+look_for = input("Enter movie title: ")
+
+for one_movie in movies:
+    if one_movie[0] == look_for:
+        print(f"Found {look_for}, {one_movie[1]} min, dir {one_movie[2]}")
