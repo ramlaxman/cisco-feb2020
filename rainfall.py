1e5e431d32f5072a35643f29a86698bcd48e6238
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-24 12:17:30 +0200

    gitautosync update 54

--- rainfall.py
+++ rainfall.py
@@ -10,3 +10,6 @@ while True:
     mm_rain = input("Rain: ").strip()
 
     rainfall[city_name] = rainfall.get(city_name, 0) + int(mm_rain)
+
+for key, value in rainfall.items():
+    print(f'{key}: {value}')
