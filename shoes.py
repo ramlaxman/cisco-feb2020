456acf06a7e94a3ea3c4c69d04707b1225972165
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-25 16:09:52 +0200

    gitautosync update 115

--- shoes.py
+++ shoes.py
@@ -13,3 +13,8 @@ shoes = [line_to_dict(one_line)
 
 for one_shoe in shoes:
     print(one_shoe)
+
+
+# (1) Sort the shoes by size (increasing order)
+# (2) Sort the shoes, first by brand and then by size (within the brand)
+# BONUS (3) Ask the user which field(s) they want to sort by, and sort by those
