38da3a09995ae1191bed8e93f4bc95f038d669a6
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-25 11:37:27 +0200

    gitautosync update 42

--- calc.py
+++ calc.py
@@ -15,3 +15,22 @@ def mul(a, b):
 
 def div(a, b):
     return a / b
+
+
+ops = {'+': add,
+       '-': sub,
+       '*': mul,
+       '/': div}
+
+while True:
+    s = input("Enter an expression: ").strip()
+
+    if not s:
+        break
+
+    first, op, second = s.split()
+
+    if op in ops:
+        print(ops[op](int(first), int(second)))
+    else:
+        print(f"No operator {op}"
