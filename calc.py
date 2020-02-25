9426bc480a1b7c1f0a3117f6badf9b87b67c3c20
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-25 11:41:55 +0200

    gitautosync update 52

--- calc.py
+++ calc.py
@@ -3,20 +3,20 @@
 import operator
 
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
 
-def sub(a, b):
-    return a - b
+# def sub(a, b):
+#     return a - b
 
 
-def mul(a, b):
-    return a * b
+# def mul(a, b):
+#     return a * b
 
 
-def div(a, b):
-    return a / b
+# def div(a, b):
+#     return a / b
 
 
 ops = {'+': operator.add,
