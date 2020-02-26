7b13def74fde90f9050fee9ada9818ee51b99dbb
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-26 10:22:19 +0200

    gitautosync update 25

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 def menu(*args):
     while True:
-        choice = input(f"Enter your choice {args}: ")
+        choice = input(f"Enter your choice {'/'.join(sorted(args))}: ")
         if choice in args:
             return choice
         print(f'Bad choice of {choice}!  Try again.')
