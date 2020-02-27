ece58a55592c4ded2aba4dbaf5cb7ddae0a19586
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-27 09:30:10 +0200

    gitautosync update 1

--- mytest.py
+++ mytest.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 
-def hello(name):
-    return f'Hello, {name}'
+def hello(name: str) -> str:
+    greeting = 3
+    greeting = 'Hello'
+    return f'{greeting}, {name}'
 
 
 print(hello('world'))
