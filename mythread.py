a8c4bf17fa9d06d8aa527758037ea5ebe89959df
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-27 15:19:52 +0200

    gitautosync update 106

--- mythread.py
+++ mythread.py
@@ -4,11 +4,14 @@ from threading import Thread
 from time import sleep
 from random import randint
 
+outputs = []
+
 
 def hello(i):
     sleep(randint(0, 2))
     print(f"{i} Hello!")
     print(f"{i} Goodbye!")
+    outputs.append(i)
 
 
 for i in range(10):
