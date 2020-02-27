2d41e71c11af4fe1538d16b0eac942ca38b34022
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-27 14:54:09 +0200

    gitautosync update 92

--- testbad.py
+++ testbad.py
@@ -8,3 +8,8 @@ Maybe even some data, on a good day.
 def myfunc():
     """Simple function that says 'Hello!'"""
     print("Hello")
+
+
+x = [10, 20, 30]
+y = [one_item*2 for one_item in x]
+print(y)
