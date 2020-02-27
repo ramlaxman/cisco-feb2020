6c4e5c05f34f423ce764ca0cde3e10cd8357e642
Reuven M. Lerner <reuven@lerner.co.il>
2020-02-27 15:17:37 +0200

    gitautosync update 103

--- mythread.py
+++ mythread.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from threading import Thread
+from time import sleep
+from random import randint
 
 
 def hello(i):
+    sleep(randint(0, 2))
     print(f"{i} Hello!")
 
 
